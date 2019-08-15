a1fa557950b1c3acd599c56e8cf066c482d885c0
masaaki-todoroki <xxx.run.to.the.sun.xxx@gmail.com>
2019-08-15 08:06:29 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotoron">
         <div class="text-center">
             <h1>welcome to the MIcroposts</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
