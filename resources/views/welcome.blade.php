4089a345a7fa89120ee4ac5616ff7c3f61d1ac8f
masaaki-todoroki <xxx.run.to.the.sun.xxx@gmail.com>
2019-08-15 09:34:29 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotoron">
-        <div class="text-center">
-            <h1>welcome to the MIcroposts</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
