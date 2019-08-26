e5da00bc6c9418ef4d92df50a736ea637146609b
masaaki-todoroki <xxx.run.to.the.sun.xxx@gmail.com>
2019-08-26 08:56:18 +0000

    cssとjsの読み込みコード修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     </head>
     <body>
         @include('commons.navbar')
@@ -20,6 +20,6 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
-        <script type="text/javascript" src="{{ asset('js/index.js') }}"></script>
+        <script type="text/javascript" src="{{ secure_asset('js/index.js') }}"></script>
     </body>
 </html>
