78c45c4dadb78fdda5227fe6321fb0691754f8b6
masaaki-todoroki <xxx.run.to.the.sun.xxx@gmail.com>
2019-08-28 11:38:10 +0000

    user一覧画面にフォローするボタンを設置

--- resources/views/user_follow/follow_button_users.blade.php
+++ resources/views/user_follow/follow_button_users.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_following($user->id))
         {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
-            {!! Form::submit('フォローを解除する', ['class' => "btn btn-danger btn-block mt10"]) !!}
+            {!! Form::submit('フォローを解除する', ['class' => "btn btn-danger"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.follow', $user->id]]) !!}
-            {!! Form::submit('フォローする', ['class' => "btn btn-primary btn-block mt10"]) !!}
+            {!! Form::submit('フォローする', ['class' => "btn btn-primary"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -3,12 +3,17 @@
         @foreach ($users as $user)
             <li class="media">
                 <img class="mr-2 rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
-                <div class="media-body">
+                <div class="media-body" style="display: flex;">
                     <div>
-                        {{ $user->name }}
+                        <div>
+                            {{ $user->name }}
+                        </div>
+                        <div>
+                            <p>{!! link_to_route('users.show', 'View profile', ['id' => $user->id]) !!}</p>
+                        </div>
                     </div>
-                    <div>
-                        <p>{!! link_to_route('users.show', 'View profile', ['id' => $user->id]) !!}</p>
+                    <div class="ml20 mt6">
+                        @include('user_follow.follow_button_users', ['user' => $user])
                     </div>
                 </div>
             </li>
